e5851517c3737358824612b1d99f4ca17c8aa1bf
Abigail Escobar <esc25862@uvg.edu.gt>
2026-02-02 17:55:11 -0600

    interface Stack completo

--- Hoja de trabajo 2/Stack.java
+++ Hoja de trabajo 2/Stack.java
@@ -1,9 +1,13 @@
 /**
- * @author
- * Fecha:
- * Propósito clase:
+ * @author Allysón Dulce Abigail Escobar 
+ * Fecha:2/2/2026
+ * Propósito clase: Define los métodos que debe de hacer, pero no cómo hacerlos, lo que lo hace más flexible al momento de implementar el cómo se hacen las acciones.
  *  
  */
-public class Stack {
+public interface Stack <T> {
+    void push(T element);
+    T pop();
+    T peek();
+    int size();
     
 }
