aae5118695ed4f35e7c96c62870258919692bbf2
Abigail Escobar <esc25862@uvg.edu.gt>
2026-02-02 18:47:41 -0600

    clases en proceso - no completas

--- Hoja de trabajo 2/ImplementacionCalculadora.java
+++ Hoja de trabajo 2/ImplementacionCalculadora.java
@@ -1,9 +1,21 @@
 /**
- * @author
- * Fecha:
+ * @author Allysón Dulce Abigail Escobar Sandoval
+ * Fecha: 
  * Propósito clase:
  *  
  */
-public class ImplementacionCalculadora {
+public class ImplementacionCalculadora implements CalculadorPostfix{
+    private Object stack;
+
+    public ImplementacionCalculadora(Stack<Integer> stack){
+        this.stack=stack;
+    }
+
+    @Override
+    public int calcular(String expresion) {
+        //falta
+        return 0;
+    }
+
     
 }
--- Hoja de trabajo 2/Principal.java
+++ Hoja de trabajo 2/Principal.java
@@ -1,9 +1,34 @@
 /**
- * @author
- * Fecha:
- * Propósito clase:
+ * @author Alyson Dulce Abigail Escobar Sandoval
+ * Fecha: 
+ * Propósito clase: permite que el programa interactue con el programa  por medio de la consola, además puede leer los archivos .txt 
  *  
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Principal {
-    
+
+    public static void main(String[] args) {
+
+        String ruta="datos.txt";
+
+        try(BufferedReader br= new BufferedReader(new FileReader(ruta))){
+            String linea;
+            while ((linea=br.readLine())!=null){
+                //falta logica
+                Object stack=null;
+                CalculadorPostfix calculador= new ImplementacionCalculadora((Stack<Integer>) stack);
+                int resultado= calculador.calcular(linea);
+                System.out.println("El resultado de " + linea + " es igual a " + resultado);
+            }
+        }
+        catch(IOException e){
+            System.out.println("Se ha encontrado un error: " + e.getMessage());
+        }
+        
+
+    }
+
 }
