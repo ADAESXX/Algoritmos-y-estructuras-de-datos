8eae7ec7cca4e42345c6eef4f7cb80ea119bdf8f
Abigail Escobar <esc25862@uvg.edu.gt>
2026-02-04 17:35:27 -0600

    version casi completa
    falta logica del vector y sus operaciones

--- Hoja de trabajo 2/Principal.java
+++ Hoja de trabajo 2/Principal.java
@@ -1,6 +1,6 @@
 /**
  * @author Alyson Dulce Abigail Escobar Sandoval
- * Fecha: 
+ * Fecha: 4/2/2026
  * Propósito clase: permite que el programa interactue con el programa  por medio de la consola, además puede leer los archivos .txt 
  *  
  */
@@ -11,15 +11,19 @@ import java.io.IOException;
 public class Principal {
 
     public static void main(String[] args) {
-
+        //ruta donde estarán los datos en formato postfix
         String ruta="datos.txt";
 
+        //
         try(BufferedReader br= new BufferedReader(new FileReader(ruta))){
+            //Variable para leer linea por linea
             String linea;
             while ((linea=br.readLine())!=null){
-                //falta logica
-                Object stack=null;
-                CalculadorPostfix calculador= new ImplementacionCalculadora((Stack<Integer>) stack);
+                //instancia del vector stack, que tiene como "padre" a la clase stack, por eso hay polimorfismo aqui
+                Stack<Integer> stack= new VectorStack<>();
+                //se envía la información del stack al constructor de la clase que lo implementará, así podrá realizar los calculos correspondientes con ese stack (stack vacio)
+                CalculadorPostfix calculador= new ImplementacionCalculadora(stack);
+                //manda la expresión parapoder calcular la respuesta usando el stack creado con anterioridad
                 int resultado= calculador.calcular(linea);
                 System.out.println("El resultado de " + linea + " es igual a " + resultado);
             }
--- Hoja de trabajo 2/VectorStack.java
+++ Hoja de trabajo 2/VectorStack.java
@@ -4,6 +4,26 @@
  * Propósito clase:
  *  
  */
-public class VectorStack {
+public class VectorStack<T> implements Stack<T>{
+
+    @Override
+    public void push(T element) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public T pop() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public T peek() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
