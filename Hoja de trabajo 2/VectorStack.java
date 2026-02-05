497bd7cb0e9c47f2f27439c8d8863cd5640ab939
PaolaItzep <itz251613@uvg.edu.gt>
2026-02-04 23:44:46 -0600

    Update VectorStack.java

--- Hoja de trabajo 2/VectorStack.java
+++ Hoja de trabajo 2/VectorStack.java
@@ -1,29 +1,62 @@
 /**
- * @author
+ * @author: Paola
  * Fecha:
  * Propósito clase:
  *  
  */
-public class VectorStack<T> implements Stack<T>{
+public class VectorStack<T> implements Stack<T> {
 
+    private Object[] vector;   
+    private int size;          
+
+  
+    public VectorStack() {
+        vector = new Object[10];
+        size = 0;
+    }
+
+    
     @Override
     public void push(T element) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (size == vector.length) {
+            resize();
+        }
+        vector[size++] = element;
     }
 
+    Elimina y retorna el elemento superior de la pila
+     */
     @Override
+    @SuppressWarnings("unchecked")
     public T pop() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (size == 0) {
+            throw new RuntimeException("Pila vacía");
+        }
+        T element = (T) vector[--size];
+        vector[size] = null;
+        return element;
     }
 
+    
     @Override
+    @SuppressWarnings("unchecked")
     public T peek() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (size == 0) {
+            throw new RuntimeException("Pila vacía");
+        }
+        return (T) vector[size - 1];
     }
 
+    
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return size;
     }
-    
+
+   
+    private void resize() {
+        Object[] newVector = new Object[vector.length * 2];
+        System.arraycopy(vector, 0, newVector, 0, vector.length);
+        vector = newVector;
+    }
 }
