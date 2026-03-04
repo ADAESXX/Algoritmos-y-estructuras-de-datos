33dacd06c09518882a51209f13c676fe7c592e26
Abigail Escobar <esc25862@uvg.edu.gt>
2026-03-03 19:15:49 -0600

    Creación archivo vacío py
    Creación del archivo de python con información relevante para empezar el trabajo

---
+++ Hoja de trabajo 5/SimulacionProcesos.py
@@ -0,0 +1,8 @@
+#Objetivos: simulacion DES (Discrete Event Simulation) y uso de colas con Resources y Container, todo ello con el modulo SimPy
+#Descripcion del programa: ----------------------------------------------------------
+#Nombre del programador: Allyson Dulce Abigail Escobar Sandoval
+#Nombre del programa: SimulacionProcesos.py
+#Fecha de creacion: 3/3/2026
+#Lenguaje: Python
+#Version de python: 3.14.0
+#Version SimPy: 4.1.1
